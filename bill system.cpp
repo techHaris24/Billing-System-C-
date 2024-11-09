ca4bd31e747a004a88bc4726c865d0c7c3f97e8d
Haris Tajammal <haris8306827@gmail.com>
2024-11-09 18:28:15 +0500

    Add files via upload

---
+++ bill system.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int precision;
+	float price1,price2,price3;
+	cout<<"Billing System"<<endl;
+	cout<<"Enter desired precision for displaying prices"<<endl;
+	cin>>precision;
+	cout<<"Enter Product 1 Price:";
+	cout<<showpoint;
+	cin>>price1;
+	cout<<"Enter Product 2 Price:";
+	cin>>price2;
+	cout<<"Enter Product 3 Price:";
+	cin>>price3;
+	cout<<"Your Total Bill is Rs."<<price1+price2+price3<<endl;
+	cout<<"Thank you for shopping!";
+}
